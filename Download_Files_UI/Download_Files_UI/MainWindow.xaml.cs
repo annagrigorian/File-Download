e47091a8f47ff7825a87e201a04e7dd685df4888
Anna Grigorian <anna.grigorian11@gmail.com>
2019-04-13 03:08:19 +0400

    Add Folder Browser

--- Download_Files_UI/Download_Files_UI/MainWindow.xaml.cs
+++ Download_Files_UI/Download_Files_UI/MainWindow.xaml.cs
@@ -1,20 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Net;
-using System.IO;
-using System.Threading;
+using System.Windows.Forms;
 
 
 namespace Download_Files_UI
@@ -30,50 +17,57 @@ namespace Download_Files_UI
             
         }
 
-        private /*async*/ void Button_ClickAsync(object sender, RoutedEventArgs e)
+        private async void Button_ClickAsync(object sender, RoutedEventArgs e)
         {
-            System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;  
-            folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
-            //using (var client = new WebClient())
-            //{
+            FolderBrowserDialog window = new FolderBrowserDialog();
+            window.Description = "Select a Folder";
+            string path = null;
+            do
+            {
+                if (window.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    path = window.SelectedPath;
+                }
+            } while (path == null);
+           
+            
+            using (var client = new WebClient())
+            {
+                downloadButton.IsEnabled = false;
 
-            //    downloadButton.IsEnabled = false;
+                try
+                {
+                    Uri url = new Uri(inputText.Text);
 
-            //    try
-            //    {
-            //        Uri url = new Uri(inputText.Text);
+                    string filename = System.IO.Path.Combine(path, System.IO.Path.GetFileName(url.ToString()));
 
-            //        string filename = System.IO.Path.Combine(Directory.GetCurrentDirectory(), System.IO.Path.GetFileName(url.ToString()));
+                    ouputText.Text = "Downloading...";
 
-            //        ouputText.Text = "Downloading...";                    
+                    await client.DownloadFileTaskAsync(url, filename);
 
-            //        await client.DownloadFileTaskAsync(url, filename);                  
-
-            //        ouputText.Text = "Downloaded!";
-            //    }
-            //    catch (UriFormatException exception)
-            //    {
-            //        ouputText.Text = exception.Message;
-            //    }
-            //    catch (WebException exception)
-            //    {
-            //        ouputText.Text = exception.Message;
-            //    }
-            //    catch (NotSupportedException exception)
-            //    {
-            //        ouputText.Text = exception.Message;
-            //    }
-            //    catch (Exception)
-            //    {
-            //        throw;
-            //    }
-            //    finally
-            //    {
-            //        downloadButton.IsEnabled = true;
-            //    }
-            //}
-
-
+                    ouputText.Text = "Downloaded!";
+                }
+                catch (UriFormatException exception)
+                {
+                    ouputText.Text = exception.Message;
+                }
+                catch (WebException exception)
+                {
+                    ouputText.Text = exception.Message;
+                }
+                catch (NotSupportedException exception)
+                {
+                    ouputText.Text = exception.Message;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    downloadButton.IsEnabled = true;
+                }
+            }
         }
     }
 }
